2287023cf7e807b9eaafd7a3a4f7bd1f904f51e4
sinoptik_ <chestas@mail.ru>
2022-12-20 23:03:54 +0300

    qwerty

--- app/src/main/java/sin/android/weather/ForecastRecyclerAdapter.kt
+++ app/src/main/java/sin/android/weather/ForecastRecyclerAdapter.kt
@@ -1,7 +1,6 @@
 package sin.android.weather
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import sin.android.weather.data.UsingWeather
